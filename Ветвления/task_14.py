72297bdf490e1575d025e2f018218776e26da58b
max <thedarthset@gmail.com>
2021-01-31 19:44:55 +0300

    решение задач 14, 15, 16

--- Ветвления/task_14.py
+++ Ветвления/task_14.py
@@ -1,3 +1,28 @@
 # По введенным длинам трех отрезков определить, можно ли из них
 # составить треугольник при условии, что у треугольника сумма любых
 # двух сторон должна быть не меньше третьей.
+
+def info_treangle():
+    a = float(input('Введите размер стороны a: '))
+    b = float(input('Введите размер стороны b: '))
+    c = float(input('Введите размер стороны c: '))
+    list_treagle = [a, b, c]
+    return list_treagle
+
+
+def treagle(data):
+    line1 = (data[0] + data[1]) > data[2]
+    line2 = (data[1] + data[2]) > data[0]
+    line3 = (data[2] + data[0]) > data[1]
+    if (line1 and line2 and line3) == True:
+        text = 'Из данных отрезков треугольник построить можно'
+    else:
+        text = 'Треугольник не пострроить'
+    return text
+
+
+def runner():
+    print(treagle(info_treangle()))
+
+
+runner()
--- Ветвления/task_15.py
+++ Ветвления/task_15.py
@@ -1,3 +1,29 @@
 # Определить, принадлежит ли точка с координатами (x; y) кругу радиуса R
 # с центром в начале координат. Пользователь вводит координаты точки и
 # радиус круга.
+
+import math
+
+
+def info_task():
+    info_ = {}
+    info_['x'] = float(input('x = '))
+    info_['y'] = float(input('y = '))
+    info_['r'] = float(input('r = '))
+    return info_
+
+
+def proverka(data):
+    g = math.sqrt(data['x'] ** 2 + data['y'] ** 2)
+    if g <= data['r']:
+        text = 'x and y in r'
+    else:
+        text = 'x and y not in r'
+    return text
+
+
+def runner():
+    print(proverka(info_task()))
+
+
+runner()
